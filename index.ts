4b7fdeafe9be503e275cdb21d3a57b08a5a41699
Anartz Mugika <mugan86@gmail.com>
2017-04-11 14:35:11 +0200

    First commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SampleComponent } from './src/utilities.component';
+import { SampleDirective } from './src/utilities.directive';
+import { CreditCardMaskPipe } from './src/pipes/credit-card-mask.pipe';
+import { SampleService } from './src/utilities.service';
+
+export * from './src/utilities.component';
+export * from './src/utilities.directive';
+export * from './src/pipes/credit-card-mask.pipe';
+export * from './src/utilities.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    SampleComponent,
+    SampleDirective,
+    CreditCardMaskPipe
+  ],
+  exports: [
+    SampleComponent,
+    SampleDirective,
+    CreditCardMaskPipe
+  ]
+})
+export class SampleModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SampleModule,
+      providers: [SampleService]
+    };
+  }
+}
---
+++ src/pipes/credit-card-mask.pipe.ts
@@ -0,0 +1,17 @@
+import { Injectable, PipeTransform, Pipe } from '@angular/core';
+
+/**
+ * Transforms any input value
+ */
+@Pipe({
+  name: 'creditCardMask'
+})
+@Injectable()
+export class CreditCardMaskPipe  implements PipeTransform {
+  transform(plainCreditCard: string): string {
+        const visibleDigits = 4;
+        let maskedSection = plainCreditCard.slice(0, -visibleDigits);
+        let visibleSection = plainCreditCard.slice(-4);
+        return maskedSection.replace(/./g, '*') + visibleSection;
+    }
+}
