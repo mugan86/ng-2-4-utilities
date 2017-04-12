490d3cd7fc437e7f3619506ca80b90a390c627df
Anartz Mugika <mugan86@gmail.com>
2017-04-12 13:44:06 +0200

    Import and export TrimPipe

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import { CreditCardMaskPipe } from './src/pipes/credit-card-mask.pipe';
 import { AllWordsCapitalizePipe } from './src/pipes/all-words-capitalize.pipe';
 import { ReverseStrPipe } from './src/pipes/reverse-str-pipe';
 import { TempConverterPipe } from './src/pipes/temp-converter.pipe';
+import { TrimPipe } from './src/pipes/trim.pipe';
 import { UtilitiesService } from './src/utilities.service';
 
 export * from './src/utilities.component';
@@ -14,7 +15,7 @@ export * from './src/pipes/credit-card-mask.pipe';
 export * from './src/pipes/all-words-capitalize.pipe';
 export * from './src/pipes/reverse-str-pipe';
 export * from './src/pipes/temp-converter.pipe';
-
+export * from './src/pipes/trim.pipe';
 export * from './src/utilities.service';
 
 @NgModule({
@@ -27,7 +28,8 @@ export * from './src/utilities.service';
     CreditCardMaskPipe,
     AllWordsCapitalizePipe,
     ReverseStrPipe,
-    TempConverterPipe
+    TempConverterPipe,
+    TrimPipe
   ],
   exports: [
     SampleComponent,
@@ -35,7 +37,7 @@ export * from './src/utilities.service';
     CreditCardMaskPipe,
     AllWordsCapitalizePipe,
     ReverseStrPipe,
-    TempConverterPipe
+    TempConverterPipe, TrimPipe
   ]
 })
 export class UtilitiesModule {
