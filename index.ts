876070866efa0bbde57011f022da18b0d3ee237f
Anartz Mugika <mugan86@gmail.com>
2017-04-12 07:52:46 +0200

    Update providers with new service name

--- index.ts
+++ index.ts
@@ -29,7 +29,7 @@ export class UtilitiesModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: UtilitiesModule,
-      providers: [SampleService]
+      providers: [UtilitiesService]
     };
   }
 }
