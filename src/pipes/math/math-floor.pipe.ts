31275dcede079ec44fcdc5b6ac4882da7460797a
Anartz Mugika <mugan86@gmail.com>
2017-04-13 10:09:40 +0200

    Update description correctly with two examples

--- src/pipes/math/math-floor.pipe.ts
+++ src/pipes/math/math-floor.pipe.ts
@@ -2,8 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 /**
  * Math.floor(x) returns the value of x rounded down to its nearest integer
- * Math.round(4.7) = 4
- * Math.round(4.4) = 4
+ * Math.floor(4.7) = 4
+ * Math.floor(4.4) = 4
  */
 
 @Pipe({name: 'mathFloor'})
