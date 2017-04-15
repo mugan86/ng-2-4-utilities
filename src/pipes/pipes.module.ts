6203e1cc6aa280bba9491a840bbc4e520358ac5f
Anartz Mugika <mugan86@gmail.com>
2017-04-15 19:27:02 +0200

    Export pipes all modules to use in global

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,25 +1,17 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
+import { NgModule, ModuleWithProviders  } from '@angular/core';
 
-
-/**
- * Custom module with all my components
- */
 import { PipesStringModule } from './str';
 import { PipesMathModule } from './math';
 import { PipesOtherModule } from './other';
 
-
-@NgModule({
-  declarations: [
-    ,
-  ],
-  imports: [
+export const PIPES = [
     PipesStringModule,
     PipesMathModule,
     PipesOtherModule
-   
-  ],
-  bootstrap: [],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+];
+
+@NgModule({
+  imports: [ PIPES ],
+  exports: [ PIPES ]
 })
 export class PipesModule { }
