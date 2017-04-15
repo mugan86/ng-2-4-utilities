317e34221e5bc8e04659407bcfe4476156ffecfe
Anartz Mugika <mugan86@gmail.com>
2017-04-15 18:56:28 +0200

    Export all string type pipes

---
+++ src/pipes/str/index.ts
@@ -0,0 +1,6 @@
+export { PipesStringModule } from './pipes-str.module';
+export { AllWordsCapitalizePipe } from './all-words-capitalize.pipe';
+export { CapitalizePipe } from './capitalize.pipe';
+export { ConcatPipe } from './concat.pipe';
+export { ReverseStrPipe } from './reverse-str.pipe';
+export { TrimPipe } from './trim.pipe';
