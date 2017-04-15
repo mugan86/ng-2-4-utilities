2a74bb64b4075a7b512ce77030af1c66aac2a3ec
Anartz Mugika <mugan86@gmail.com>
2017-04-15 15:37:41 +0200

    Export all math pipes

---
+++ src/pipes/math/index.ts
@@ -0,0 +1,8 @@
+export { PipesMathModule } from './pipes-math.module';
+export { MathAbsPipe } from './abs.pipe';
+export { MathCeilPipe } from './ceil.pipe';
+export { MathFloorPipe } from './floor.pipe';
+export { MathPowPipe } from './pow.pipe';
+export { MathRoundPipe } from './round.pipe';
+export { MathSqrtPipe } from './sqrt.pipe';
+
