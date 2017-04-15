52a2d4640775178d8697a5bab0c07b68790cf979
Anartz Mugika <mugan86@gmail.com>
2017-04-15 15:12:49 +0200

    Export from module and directives

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,25 +1,3 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-
-import { DefaultImage } from './default-image.directive';
-import { SampleDirective } from './utilities.directive';
-
-export * from './default-image.directive';
-export * from './utilities.directive';
-
-export const DIRECTIVES = [
-  DefaultImage,
-  SampleDirective
-];
-
-@NgModule({
-  declarations: DIRECTIVES,
-  exports: DIRECTIVES,
-  bootstrap:    DIRECTIVES
-})
-export class DirectivesModule { 
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: DirectivesModule,
-    };
-  }
-}
+export {DirectivesModule} from './directives.module';
+export {DefaultImage} from './default-image.directive';
+export {SampleDirective} from './utilities.directive';
