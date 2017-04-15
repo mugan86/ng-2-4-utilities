5e0fa790e9831fc1af03c2d2ff528707bc328940
Anartz Mugika <mugan86@gmail.com>
2017-04-15 18:56:50 +0200

    Export all other type pipes

---
+++ src/pipes/other/index.ts
@@ -0,0 +1,4 @@
+export { PipesOtherModule } from './pipes-other.module';
+export { CreditCardMaskPipe } from './credit-card-mask.pipe';
+export { TempConverterPipe } from './temp-converter.pipe';
+export { WikipediaMobileUrlPipe } from './wikipedia-mobile-url.pipe';
