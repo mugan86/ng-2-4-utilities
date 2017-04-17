1c7246b293597d6cb617d5be95f934fd8261e699
Anartz Mugika <mugan86@gmail.com>
2017-04-17 19:05:22 +0200

    Only apply credit card mask in 13-19 chars length numbers

--- src/pipes/other/credit-card-mask.pipe.ts
+++ src/pipes/other/credit-card-mask.pipe.ts
@@ -8,10 +8,16 @@ import { Injectable, PipeTransform, Pipe } from '@angular/core';
 })
 @Injectable()
 export class CreditCardMaskPipe  implements PipeTransform {
-  transform(plainCreditCard: string): string {
+    transform(plainCreditCard: string): string {
+
+      if (plainCreditCard.length >= 13 && plainCreditCard.length<=19)
+      {
         const visibleDigits = 4;
         let maskedSection = plainCreditCard.slice(0, -visibleDigits);
         let visibleSection = plainCreditCard.slice(-4);
         return maskedSection.replace(/./g, '*') + visibleSection;
+      }
+      return "Credit card number contain 13-19 chars";
+        
     }
 }
