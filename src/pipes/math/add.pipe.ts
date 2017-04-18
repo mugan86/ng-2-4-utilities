35dc7137ebc84a567c87f3b1b89c59968094bb6a
Anartz Mugika <mugan86@gmail.com>
2017-04-18 11:45:24 +0200

    Add sum pipe

---
+++ src/pipes/math/add.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+/**
+ *  returns the absolute (positive) value of x
+ * Math.abs(-4.7) = 4.7
+ * Math.abs(4.4) = 4.4
+ */
+
+@Pipe({name: 'mathSum'})
+export class MathSumPipe implements PipeTransform {
+  transform(value: number, values: number[]): any {
+    if (value && !isNaN(value)) this.makeAllSums(value, values);
+    return -1;
+  }
+
+  makeAllSums(startValue:number, numbersToSum: number[])
+  {
+    for (let i = 0; i < numbersToSum.length; i++)
+    {
+        startValue = startValue + numbersToSum[i];
+    }
+    return startValue;
+  }
+}
