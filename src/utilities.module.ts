ba1564ffbc67dc695d836e5ec51a990ecc4ec2f5
Anartz Mugika <amugika@codesyntax.com>
2017-05-23 14:30:56 +0200

    Import translate

--- src/utilities.module.ts
+++ src/utilities.module.ts
@@ -3,13 +3,27 @@ import { NgModule, ModuleWithProviders  } from '@angular/core';
 import { PipesModule } from './pipes';
 import { DirectivesModule } from './directives';
 
+// To Translate imports
+import { TranslateModule, TranslateLoader  } from '@ngx-translate/core';
+import { createTranslateLoader } from './services/translate-loader';
+import { Http } from '@angular/http';
+
 export const MODULES = [
     PipesModule,
     DirectivesModule
 ];
 
 @NgModule({
-  imports: [ MODULES ],
+  imports:
+  [  MODULES,
+              TranslateModule.forRoot({
+                loader: {
+                  provide: TranslateLoader,
+                  useFactory: (createTranslateLoader),
+                  deps: [Http]
+                }
+              })
+  ],
   exports: [ MODULES ]
 })
 export class UtilitiesModule { }
